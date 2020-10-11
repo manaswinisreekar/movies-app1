92930de46bc6f3c1e42b9c039fb2590fc04f9cca
manaswinisreekar <you@example.com>
2020-10-11 21:59:06 +0530

    modified changes in the header logo

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import './Headers.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
 
  
-class header extends Component{
+class Header extends Component{
     render(){
         return(
             <div>
+                <header className="app-header">
+                    <img src={logo}className="app-logo" alt="Movie App Logo"/>
+                </header>
                 <Button variant="contained" color="default">Login</Button>
             </div>
         )
